c70d1b1f540f79d7e33cb8af7f9bb1cf66a79ecb
Đặng Quang Anh <42268330+Anh1223@users.noreply.github.com>
2018-09-11 21:56:58 +0700

    Add files via upload

---
+++ Seesion 9/life_of_TechKids.py
@@ -0,0 +1,83 @@
+from combat import *
+
+Orc = {
+    "NAME": "MiniOrc",
+    "CLASS": "Orc",
+    "HP": 60,
+    "STR": 5,
+    "DEF": 2,
+    "LUCK": 2,
+    "AGI": 1,
+    "LVL": 1,
+}
+player = {
+    "NAME": "Anh1223",
+    "CLASS": "Hacker",
+    "HP": 60,
+    "STR": 7,
+    "DEF": 3,
+    "LUCK": 5,
+    "AGI": 2,
+    "LVL": 1,
+}
+while True:
+    cmd = input("Your command(Stats or Here)")
+    cmd = cmd.lower()
+    if cmd == "stats":
+         show_index(player)
+    elif cmd == "here":
+        print("Bạn đang ở trước cửa TechKids")
+        print("Bạn có 2 lựa chọn")
+        print("1. Về TechKids")
+        print("2. Đi vào khu rừng phía trước")
+        option = input("Lựa chọn của bạn ?")
+        if option == "1":
+            print("Xin lỗi, đã hết giờ làm việc")
+        elif option == "2":
+            print("Bạn đã bước vào rừng")
+            print("Bạn thấy một bình thủy dịch ở trên mặt đất")
+            print("1. Bỏ qua")
+            print("2. Nhặt lên sử dụng")
+            option = input("Lựa chọn bạn")
+            if option == "1":
+                print("Tiếc quá")
+                print("Bạn gặp 1 con Orc")
+                print("1. Chạy ra khỏi khu vực này")
+                print("2. Nấp vào hang đá bên cạnh")
+                print("3. ĐÁNH!!!")
+                option = input("Lựa chọn của bạn?")
+                if option == "1":
+                    print("Do bạn chạy quá chậm nên đã bị Orc bắt đi")
+                elif option == "2":
+                    print("Con Orc không nhìn thấy")
+                    print("Tuy nhiên bạn quay lại nhìn vào trong hanh, bạn thấy một đoàn sói")
+                elif option == "3":
+                    print("OPPONENT")
+                    print(Orc)
+
+            elif option == "2":
+                player["HP"] = 100
+                print("Bạn đã được hồi phục HP")
+                print("HP", player["HP"])
+                print("Bạn gặp 1 con Orc")
+                print("1. Chạy ra khỏi khu vực này")
+                print("2. Nấp vào hang đá bên cạnh")
+                print("3. ĐÁNH!!!")
+                option = input("Lựa chọn của bạn?")
+                if option == "1":
+                    print("Do bạn chạy quá chậm nên đã bị Orc bắt đi")
+                elif option == "2":
+                    print("Con Orc không nhìn thấy bạn")
+                    print("Tuy nhiên bạn quay lại nhìn vào trong hanh, bạn thấy một đàn sói")
+                elif option == "3":
+                    print("OPPONENT")
+                    use_item(player)
+                    print(player)
+                    show_index(Orc)
+                    combat_full(player, Orc)
+
+
+
+
+    else :
+        print("Hãy nhập lại")
